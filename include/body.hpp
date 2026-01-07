3084f11322e86627df879c1a79a709e6b6223038
TyBrassington <98743450+TyBrassington@users.noreply.github.com>
2026-01-06 22:53:49 -0800

    chore: strip all unneeded code for gravity sim from old code

---
+++ include/body.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "math.hpp"
+
+struct Body {
+    Vec3D position;
+    Vec3D velocity;
+    float mass;
+    float radius;
+};
+
--- include/obj_loader.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-#include "mesh.hpp"
-
-bool loadOBJ(const std::string& path, Mesh& mesh);
-bool loadMTL(const std::string& path,
-             std::unordered_map<std::string, int>& matIndex,
-             std::vector<Material>& materials);
